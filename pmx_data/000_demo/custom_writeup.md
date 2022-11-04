2679c7c0e79ed70a6361a6ec3894fe570b6893b1
Merritt Kowaleski <mkowales@andrew.cmu.edu>
2022-11-04 15:58:35 -0400

    demo

---
+++ pmx_data/000_demo/custom_writeup.md
@@ -0,0 +1,4 @@
+# demo header
+lsakhflfdjhflks
+;hdsalkfdsjhah
+;lhsfjkd
---
+++ pmx_data/000_demo/get_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#download data zip file from the hosting site using wget
+wget https://prod-dcd-datasets-cache-zipfiles.s3.eu-west-1.amazonaws.com/fddp3dvvzr-1.zip
+unzip fddp3dvvzr-1.zip
+rm fddp3dvvzr-1.zip
+mv DeltaRobot_ParameterReadings datasets
+
