3b956c97f1fe412f37dfe32f360275828d038845
Merritt Kowaleski <mkowales@andrew.cmu.edu>
2022-11-16 15:17:36 -0500

    modify delta_robot and diesel_engine_faults

--- pmx_data/diesel_engine_faults/get_data.sh
+++ pmx_data/diesel_engine_faults/get_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://md-datasets-cache-zipfiles-prod.s3.eu-west-1.amazonaws.com/k22zxz29kr-1.zip
+wget https://prod-dcd-datasets-cache-zipfiles.s3.eu-west-1.amazonaws.com/k22zxz29kr-1.zip
 unzip -d datasets k22zxz29kr-1.zip
 rm k22zxz29kr-1.zip
--- pmx_data/diesel_engine_faults/load_data.py
+++ pmx_data/diesel_engine_faults/load_data.py
@@ -2,13 +2,24 @@ import scipy.io
 import os
 import pandas as pd
 
+#I'm not sure how to extract this data in a sensible way
+#finding a correspondence between the description of the dataset
+#and the actual content is difficul
+#I'm going to leave this for now and com back to it
+
 datasets = {}
 for dataset in os.listdir("datasets"):
 	dataset_path = os.path.join("datasets", dataset)
 	dataset_name = dataset.split(".")[0]
 	print(dataset_name)
 	mat = scipy.io.loadmat(dataset_path)
+	print(mat.keys())
+	print(mat['__header__'])
+	print(mat['__version__'])
+	print(mat['__globals__'])
+	print(mat['None'])
 	mdata = mat['__function_workspace__']
+	print(mdata)
 	datasets[dataset_name] = mdata[0]
 
 df = pd.DataFrame.from_dict(datasets)
