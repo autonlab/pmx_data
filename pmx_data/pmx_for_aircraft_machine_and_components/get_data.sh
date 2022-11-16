fdf85dc2a65602e978a2ad5e1d7e6e3d23996a8a
Merritt Kowaleski <mkowales@andrew.cmu.edu>
2022-11-16 16:34:04 -0500

    update all kaggle datasets with note

--- pmx_data/pmx_for_aircraft_machine_and_components/get_data.sh
+++ pmx_data/pmx_for_aircraft_machine_and_components/get_data.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-#Requires kaggle api, see https://github.com/Kaggle/kaggle-api for installation and setup instructions
+#Requires kaggle api
+#see https://github.com/Kaggle/kaggle-api for installation and setup instructions
+#or follow below instructions:
+
+#Ensure you have Python 3 and the package manager pip installed.
+#run: pip install kaggle
+#sign up for a Kaggle account at https://www.kaggle.com
+#go to the 'Account' tab of your user profile (https://www.kaggle.com/<username>/account)
+#select 'Create API Token'.
+#This will trigger the download of kaggle.json, a file containing your API credentials. 
+#Place this file in the location ~/.kaggle/kaggle.json
+
 kaggle datasets download -p datasets --unzip arnabbiswas1/microsoft-azure-predictive-maintenance
