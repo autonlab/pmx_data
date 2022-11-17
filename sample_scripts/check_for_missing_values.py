39741d9a06fe15fa49a7cc5f3975b657a469e671
Merritt Kowaleski <mkowales@andrew.cmu.edu>
2022-11-17 14:28:08 -0500

    improve check for missing values

--- sample_scripts/check_for_missing_values.py
+++ sample_scripts/check_for_missing_values.py
@@ -1,27 +1,34 @@
 import os
 import pandas as pd
+import sys
 #run this once you've downloaded your dataset to check if any of the csv's have missing values
-#it will look through all the subfolders recursively and print a message if they have missing values in any of their files
-#if there are no missing values, it will not print anything.
+#it will look through all the subfolders recursively 
+#and will print "True" if any csv has missing data and "False" otherwise
+#to run: python check_for_missing_values.py /path/to/dataset
 
-def make_data_dict(path):
-	missingvalues = False
-	data_dict = {}
-	
+def has_missing_data(path):
 	for name in os.listdir(path):
 		name_path = os.path.join(path, name)
 		
-		#for each directory, make a sub-dictionary
-		#for each csv, import it as a dataframe
 		if os.path.isdir(name_path):
-			data_dict[name] = make_data_dict(name_path)
-		else:
-			dataset_name = name.split(".")[0]
-			data_dict[dataset_name] = pd.read_csv(name_path)
-			if not missingvalues: 
-				missingvalues = data_dict[dataset_name].isnull().values.any()
-	if missingvalues:
-		print("Null values in " + path)
-	return data_dict
+			if has_missing_data(name_path):
+				return True
 
-datasets = make_data_dict("datasets")
+		elif name.split(".")[1] == "csv":
+			dataframe = pd.read_csv(name_path)
+			if dataframe.isnull().values.any():
+				return True
+
+		else:
+			#ignore non-csv files
+			pass
+
+	return False
+
+try:
+	path = sys.argv[1]
+except:
+	#if run with no arguments, assume it's being run in the data folder
+	path = "datasets"
+
+print(has_missing_data(path))
